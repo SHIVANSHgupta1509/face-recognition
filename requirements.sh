811e9ad157014119022683775ad21b2d5f8038c7
SHIVANSH GUPTA <shivanshgupta1509@gmail.com>
2022-05-29 21:03:41 +0530

    added requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,15 @@
+# Python3 must be installed in the system. 
+
+
+# In addition install the required libraries by the below commands.
+
+
+pip install tk-tools
+pip install opencv-contrib-python
+pip install datetime
+pip install pytest-shutil
+pip install python-csv
+pip install numpy
+pip install pillow 
+pip install pandas
+pip install times
